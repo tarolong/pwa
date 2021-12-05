0e5c52bd9f7697136ee3c7dde1c0f9c68ac824ef
tarolong <88961791+tarolong@users.noreply.github.com>
2021-12-05 23:16:19 +0900

    최초

---
+++ precache-manifest.c478479cc28d1e3a4f3affe58a48553d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00c882c763cdf8572def",
+    "url": "/pwa/css/app.c9ddb99c.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/pwa/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "9f2571e87b40de51a61eca81b67901a8",
+    "url": "/pwa/index.html"
+  },
+  {
+    "revision": "00c882c763cdf8572def",
+    "url": "/pwa/js/app.e22f9180.js"
+  },
+  {
+    "revision": "ce5915ea98646660f61a",
+    "url": "/pwa/js/chunk-vendors.5481cc38.js"
+  },
+  {
+    "revision": "2aa44c71330a8dd79fbe42c4cfc9760d",
+    "url": "/pwa/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/pwa/robots.txt"
+  }
+]);
